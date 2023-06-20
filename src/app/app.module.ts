20a870f3dd7f896d7edb721f9f441dc42300d7a0
Brian Aguilar <bmaguilar03@gmail.com>
2023-06-20 08:59:06 -0400

    Fixed errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateFoodComponent } from './create-food/create-food.component';
 import { FoodDetailsComponent } from './food-details/food-details.component';
 import { FoodListComponent } from './food-list/food-list.component';
 import { UpdateFoodComponent } from './update-food/update-food.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -23,7 +27,10 @@ import { UpdateFoodComponent } from './update-food/update-food.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
